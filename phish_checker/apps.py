44213c15573ff18265e202a6ae2fc0bb90f1a4af
propersam <a.samuel@suretrade.ng>
2018-10-24 10:45:34 +0100

    Setting saved model in backend

--- phish_checker/apps.py
+++ phish_checker/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
-
+from sklearn.linear_model import joblib
 
 class PhishCheckerConfig(AppConfig):
     name = 'phish_checker'
+
+
+def load_model():
+	filename = 'trained_model.sav'
+	loaded_model = joblib.load(filename)
--- phish_checker/views.py
+++ phish_checker/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
+from .apps import load_model
 # Create your views here.
 
 def index(request):
 
     # context = {'home_page': home_page}
     return render(request, 'phish_checker/index.html')
+
+def model_predict(request):
+
+	model = load_model.loaded_model
+
+	return render(request, 'WE are moving on')
